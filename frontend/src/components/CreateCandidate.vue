f2f1de9cf098b20d50c872b8f0e3e2d75901def6
Bogdan Kascelan <bogdan.kascelan@student.um.si>
2023-05-18 01:08:26 +0200

    crud for candidates and start of admin page

--- frontend/src/components/CreateCandidate.vue
+++ frontend/src/components/CreateCandidate.vue
@@ -1,57 +1,93 @@
 <template>
-    <div>
-      <h2>Create Candidate List</h2>
-      <form @submit.prevent="createList">
-        <div v-for="(candidate, index) in candidates" :key="index">
-          <h3>Candidate {{ index + 1 }}</h3>
-          <label>First Name:</label>
-          <input type="text" v-model="candidate.firstName" required><br><br>
-          <label>Last Name:</label>
-          <input type="text" v-model="candidate.lastName" required><br><br>
-          <label>Study Program:</label>
-          <input type="text" v-model="candidate.studyProgram" required><br><br>
-          <label>University:</label>
-          <input type="text" v-model="candidate.university" required><br><br>
-          <label>Faculty:</label>
-          <input type="text" v-model="candidate.faculty" required><br><br>
-          <label>Student ID:</label>
-          <input type="text" v-model="candidate.studentId" required><br><br>
-          <label>Email:</label>
-          <input type="email" v-model="candidate.email" required><br><br>
-          <hr>
-        </div>
-        <button type="submit">Create List</button>
-      </form>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        candidates: [{ firstName: '', lastName: '', studyProgram: '', university: '', faculty: '', studentId: '', email: '' }]
+  <form class="register-form" @submit.prevent="submitForm">
+    <label for="name">Candidate Name:</label>
+    <input type="text" id="name" class="input-field" v-model="candidate.name">
+
+    <label for="studyDirection">Study Direction:</label>
+    <input type="text" id="studyDirection" class="input-field" v-model="candidate.studyDirection">
+
+    <label for="enrollmentNumber">Enrollment Number:</label>
+    <input type="text" id="enrollmentNumber" class="input-field" v-model="candidate.enrollmentNumber">
+
+    <label for="email">Email:</label>
+    <input type="text" id="email" class="input-field" v-model="candidate.email">
+
+    <label for="university">University:</label>
+    <select id="university" class="input-field" v-model="candidate.university">
+      <option v-for="university in universities" :key="university.id" :value="university.id">
+        {{ university.name }}
+      </option>
+    </select>
+
+    <label for="faculty">Faculty:</label>
+    <select id="faculty" class="input-field" v-model="candidate.faculty">
+      <option v-for="faculty in faculties" :key="faculty.id" :value="faculty.id">
+        {{ faculty.name }}
+      </option>
+    </select>
+
+    <label for="studyProgram">Study Program:</label>
+    <select id="studyProgram" class="input-field" v-model="candidate.studyProgram">
+      <option v-for="studyProgram in studyPrograms" :key="studyProgram.id" :value="studyProgram.id">
+        {{ studyProgram.name }}
+      </option>
+    </select>
+
+    <button type="submit" class="submit-btn">Create Candidate</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      candidate: {
+        name: '',
+        studyDirection: '',
+        university: '',
+        faculty: '',
+        studyProgram: '',
+        email: '', // New field
+        enrollmentNumber: '', // New field
+      },
+      universities: [],
+      faculties: [],
+      studyPrograms: [],
+    };
+  },
+  async created() {
+    try {
+      const universitiesResponse = await axios.get('http://localhost:3000/university');
+      this.universities = universitiesResponse.data;
+
+      const facultiesResponse = await axios.get('http://localhost:3000/faculty');
+      this.faculties = facultiesResponse.data;
+
+      const studyProgramsResponse = await axios.get('http://localhost:3000/studyprogram');
+      this.studyPrograms = studyProgramsResponse.data;
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  methods: {
+    async submitForm() {
+      try {
+        await axios.post('http://localhost:3000/candidate/create', this.candidate);
+        this.candidate.name = '';
+        this.candidate.studyDirection = '';
+        this.candidate.university = '';
+        this.candidate.faculty = '';
+        this.candidate.studyProgram = '';
+        this.candidate.email = '';
+        this.candidate.enrollmentNumber = ''; 
+        alert('Candidate created successfully!');
+      } catch (error) {
+        console.error(error);
       }
     },
-    methods: {
-      async createList() {
-        try {
-          const response = await fetch('/api/candidate-list', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(this.candidates)
-          });
-          if (response.ok) {
-            alert('Candidate list created successfully!');
-            this.candidates = [{ firstName: '', lastName: '', studyProgram: '', university: '', faculty: '', studentId: '', email: '' }];
-          } else {
-            alert('Error creating candidate list');
-          }
-        } catch (error) {
-          console.error(error);
-          alert('Error creating candidate list');
-        }
-      }
-    }
-  }
-  </script>
-  
+  },
+};
+</script>
+<style src="../css/candidate.css" scoped></style>
