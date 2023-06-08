589ad4b04d20a5400ba27a93fc595ce59f660b04
Bogdan Kascelan <bogdan.kascelan@student.um.si>
2023-06-08 08:22:36 +0200

    keep alive implementation

--- backend/db.js
+++ backend/db.js
@@ -2,15 +2,41 @@ const mysql = require('mysql');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host     : process.env.DB_HOST,
-  user     : process.env.DB_USER,
-  password : process.env.DB_PASSWORD,
-  database : process.env.DB_DATABASE
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 });
 
-connection.connect(error => {
-  if (error) throw error;
+connection.connect((error) => {
+  if (error) {
+    console.error('Error connecting to the database:', error);
+    throw error;
+  }
   console.log('Successfully connected to the database.');
+
+  // Send a ping request to the database to keep the connection alive
+  connection.ping((pingError) => {
+    if (pingError) {
+      console.error('Error pinging the database:', pingError);
+      // Handle the error if needed
+    } else {
+      console.log('Database connection is alive.');
+    }
+  });
+
+  // Schedule the ping to be called every 5 minutes
+  setInterval(() => {
+    connection.ping((pingError) => {
+      if (pingError) {
+        console.error('Error pinging the database:', pingError);
+        // Handle the error if needed
+      } else {
+        console.log('Database connection is alive.');
+      }
+    });
+  }, 100000); // 5 minutes interval (in milliseconds)
 });
 
+// Export the connection for other modules to use
 module.exports = connection;
